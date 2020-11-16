0975d45147a880f9d6a447f52c1fca05377c62ac
harsha <29434052+harshare@users.noreply.github.com>
2020-11-16 17:33:16 +0000

    MMCA-2490 Delete redundant request method

--- src/test/scala/uk/gov/hmrc/perftests/cashaccount/Requests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/cashaccount/Requests.scala
@@ -22,21 +22,12 @@ import io.gatling.http.request.builder.HttpRequestBuilder
 
 object Requests {
 
-  def getPage(stepName: String, saveToken: Boolean, url: String): HttpRequestBuilder = {
+  def getPage(stepName: String, url: String): HttpRequestBuilder = {
     val httpRequestBuilder = http("Get " + stepName)
       .get(url)
       .check(status.is(200))
       .check(currentLocation.is(url))
-    if (saveToken) {
-      httpRequestBuilder.check(css("input[name='csrfToken']", "value").saveAs("csrfToken"))
-    }
-    else {
       httpRequestBuilder
-    }
-  }
-
-  def getPage(stepName: String, url: String): HttpRequestBuilder = {
-    getPage(stepName, saveToken = false, url)
   }
 
   def postPage(stepName: String, currentPage: String, nextPage: String, value: String): HttpRequestBuilder =
