474a24c06af36bd9fd931f449094ba1fe520325b
jamie2210 <116884495+jamie2210@users.noreply.github.com>
2025-01-22 10:39:58 +0000

    [MMCA-5101] - Remove com.typesafe

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,6 @@ import sbt.*
 object Dependencies {
 
   val test = Seq(
-    "com.typesafe"          % "config"                    % "1.4.3"     % Test,
     "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"     % Test,
   )
 }
